06ef6ca09de7f36b357c600ec3433d731577173d
david chinchin <dmquito@hotmail.com>
2023-11-19 16:18:38 -0500

    feat: add model admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from recipes.models import Recipe, Ingredient, Step
+
 # Register your models here.
+@admin.register(Recipe)
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'dificulty', 'likes', 'bookmarks',]
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ['recipe', 'step',]
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['recipe', 'quantity', 'description',]
